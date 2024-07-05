92b0420eccd973a75f5fc2983181304962d8a533
yamelsenih <ysenih@erpya.com>
2024-07-05 17:36:35 -0400

    Add backward compatibility using attachments

--- src/main/java/org/spin/eca62/support/IS3.java
+++ src/main/java/org/spin/eca62/support/IS3.java
@@ -1,8 +1,7 @@
 /******************************************************************************
  * Product: Adempiere ERP & CRM Smart Business Solution                       *
  * This program is free software; you can redistribute it and/or modify it    *
- * under the terms version 2 or later of the                                  *
- * GNU General Public License as published                                    *
+ * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
@@ -11,18 +10,34 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
  * For the text or an alternative of this public license, you may reach us    *
- * Copyright (C) 2003-2019 E.R.P. Consultores y Asociados, C.A.               *
+ * Copyright (C) 2003-2015 E.R.P. Consultores y Asociados, C.A.               *
  * All Rights Reserved.                                                       *
  * Contributor(s): Yamel Senih www.erpya.com                                  *
  *****************************************************************************/
-package org.spin.template.util;
+package org.spin.eca62.support;
+
+import java.io.InputStream;
 
 /**
- * Add here all changes for core and statci methods
- * Please rename this class and package
- * @author Yamel Senih, ysenih@erpya.com, ERPCyA http://www.erpya.com
+ * S3 interface, this can help to implement methods like shared URL
+ * @author Yamel Senih, ySenih@erpya.com, ERPCyA http://www.erpya.com
+ * @see https://docs.aws.amazon.com/AmazonS3/latest/dev/ObjectOperations.html
  */
-public class Changes {
-	/**	Colum added to core	*/
-	public static final String COLUMNNAME_ColumAddedToCore = "ColumAddedToCore";
+public interface IS3 {
+	
+	/**
+	 * Put resource based on metadata (Client, User, container Type, Table, Record ID)
+	 * @param resourceMetadata
+	 * @param resource
+	 * @throws Exception
+	 */
+	public String putResource(ResourceMetadata resourceMetadata, InputStream resource) throws Exception;
+	
+	/**
+	 * Put resource based on metadata (Client, User, container Type, Table, Record ID)
+	 * @param resourceMetadata
+	 * @return
+	 * @throws Exception
+	 */
+	public InputStream getResource(ResourceMetadata resourceMetadata) throws Exception;
 }
